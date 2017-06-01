52c5b1eff0dbeabdea4e10886aef5b1357a120b7
Larisa Ustalov <larisau@scylladb.com>
2017-06-01 15:43:03 +0300

    added support for protocol v4 testing

--- run.py
+++ run.py
@@ -8,11 +8,12 @@ import processjunit
 
 class Run:
 
-    def __init__(self, python_driver_git, scylla_install_dir, tag, tests):
+    def __init__(self, python_driver_git, scylla_install_dir, tag, protocol, tests):
         self._tag = tag
         self._python_driver_git = python_driver_git
         self._scylla_install_dir = scylla_install_dir
         self._tests = tests
+        self._protocol = protocol
         self._xunit_file = self._get_xunit_file(self._setup_out_dir())
         self._run()
         self._junit = self._process_output()
@@ -22,35 +23,42 @@ class Run:
         return self._junit.summary
 
     def __repr__(self):
-        details = dict(version=self._tag)
+        details = dict(version=self._tag, protocol=self._protocol)
         details.update(self._junit.summary)
-        return '{version}: testcases: {testcase}, failures: {failure}, errors: {error}, skipped: {skipped},' \
-               ' ignored_in_analysis: {ignored_in_analysis}'.format(**details)
+        return '{version}: v{protocol}: testcases: {testcase},' \
+            ' failures: {failure}, errors: {error}, skipped: {skipped},' \
+            ' ignored_in_analysis: {ignored_in_analysis}'.format(**details)
 
     def _setup_out_dir(self):
         here = os.path.dirname(__file__)
         xunit_dir = os.path.join(here, 'xunit', self._tag)
-        if os.path.exists(xunit_dir):
-            shutil.rmtree(xunit_dir)
-        os.makedirs(xunit_dir)
+        if not os.path.exists(xunit_dir):
+            os.makedirs(xunit_dir)
         return xunit_dir
 
     def _get_xunit_file(self, xunit_dir):
-        return os.path.join(xunit_dir, 'nosetests.{}.xml'.format(self._tag))
+        file_path = os.path.join(xunit_dir, 'nosetests.v{}.{}.xml'.format(self._protocol, self._tag))
+        if os.path.exists(file_path):
+            os.unlink(file_path)
+        return file_path
 
     def _ignoreFile(self):
         here = os.path.dirname(__file__)
         return os.path.join(here, 'versions', self._tag, 'ignore.yaml')
 
     def _ignoreSet(self):
+        ignore_tests = []
         with open(self._ignoreFile()) as f:
             content = yaml.load(f)
-            return set(content['tests'])
+            ignore_tests.extend(content['tests'])
+            if self._protocol == '4' and 'v4_tests' in content and content['v4_tests']:
+                ignore_tests.extend(content['v4_tests'])
+        return set(ignore_tests)
 
     def _environment(self):
         result = {}
         result.update(os.environ)
-        result['PROTOCOL_VERSION'] = '3'
+        result['PROTOCOL_VERSION'] = self._protocol
         result['INSTALL_DIRECTORY'] = self._scylla_install_dir
         return result
 
@@ -78,5 +86,6 @@ class Run:
     def _process_output(self):
         junit = processjunit.ProcessJUnit(self._xunit_file, self._ignoreFile())
         content = open(self._xunit_file).read()
-        open(self._xunit_file, 'w').write(content.replace('classname="', 'classname="version_{}_'.format(self._tag)))
+        open(self._xunit_file, 'w').write(content.replace('classname="', 'classname="version_{}_v{}_'.format(
+            self._tag, self._protocol)))
         return junit
